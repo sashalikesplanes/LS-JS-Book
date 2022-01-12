7255f82809de22f9b8f32b7475a41c66425ac2fa
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-12 15:48:36 -0500

    solved ex4

---
+++ funcs/ex4.js
@@ -0,0 +1,9 @@
+function scream(words) {
+  words = words + '!!!!';
+  return;
+  console.log(words);
+}
+
+scream('Yipeee');
+
+// Logs nothing to console as the return statement makes the func scream stop executing
