e39a1e3d02d63ce21067b8224760dc42ee40cf5c
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-12 14:59:49 -0500

    code example

---
+++ funcs/greeting.js
@@ -0,0 +1,12 @@
+function greetPeople() {
+  let greetingMessage = 'Good Morning!';
+  console.log(greetingMessage);
+}
+
+/*
+function changeGreetingMsg() {
+  greetingMessage = 'Good eve';
+}*/
+
+greetPeople();
+console.log(greetingMessage);
