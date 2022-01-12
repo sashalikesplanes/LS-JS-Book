3c03d674baf3d652a58e9c5a277375ba92f479f5
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-12 09:10:10 -0500

    solved ex2

---
+++ vars/age.js
@@ -0,0 +1,6 @@
+const currentAge = 21;
+
+console.log(`In 10 years, you will be ${currentAge + 10} years old.`);
+console.log(`In 20 years, you will be ${currentAge + 20} years old.`);
+console.log(`In 30 years, you will be ${currentAge + 30} years old.`);
+console.log(`In 40 years, you will be ${currentAge + 40} years old.`);
