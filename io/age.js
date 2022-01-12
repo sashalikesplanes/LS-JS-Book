9ec22721e500ba673c6110e0fd1188755c1c38eb
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-12 09:35:17 -0500

    solved ex3

---
+++ io/age.js
@@ -0,0 +1,9 @@
+let rlSync = require('readline-sync');
+let currentAge = rlSync.question('How old are you? ');
+currentAge = parseInt(currentAge);
+
+console.log(`You are ${currentAge} years old.`)
+console.log(`In 10 years, you will be ${currentAge + 10} years old.`);
+console.log(`In 20 years, you will be ${currentAge + 20} years old.`);
+console.log(`In 30 years, you will be ${currentAge + 30} years old.`);
+console.log(`In 40 years, you will be ${currentAge + 40} years old.`);
