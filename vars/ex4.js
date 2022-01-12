f9e8d201f0673f4f9077a73e2526ed372ea4933f
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-12 09:13:45 -0500

    solved ex4

---
+++ vars/ex4.js
@@ -0,0 +1,12 @@
+const NAME = 'Victor';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
+NAME = 'Joe';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
+// We get an error on line 6 as NAME is created as a const but then we attempt
+// to reasign the const variable which is illegal
