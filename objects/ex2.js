1e6c2f12203d41b27c893e9e5d6dbdd3ff7ca865
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-13 11:46:15 -0500

    solved ex1 and ex2

---
+++ objects/ex2.js
@@ -0,0 +1,8 @@
+1 // invalid key
+'1' // valid key
+undefined // invalid key
+'hello world' // valid key
+true // invalid key
+'true' // valid key
+
+// CORRECTION all keys will be coerced into strings
