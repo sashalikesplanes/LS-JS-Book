ab423c08d52d1f6b2d4ac8d081a1d7fd928a13c4
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-13 11:01:12 -0500

    solved last ex

---
+++ loops-n-iters/factorialRecursion.js
@@ -0,0 +1,14 @@
+const factorial = n => {
+  // Returns the factorial of n
+  if (n < 3) return n;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
