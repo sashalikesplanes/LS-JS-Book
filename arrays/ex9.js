a1411c84ad015b2491e31a230a42e5f569324321
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-13 11:33:44 -0500

    solved ex9

---
+++ arrays/ex9.js
@@ -0,0 +1,9 @@
+let numbers1 = [1, 3, 5, 7, 9, 11];
+let numbers2 = [];
+let numbers3 = [2, 4, 6, 8];
+
+const checkForThree = arr => arr.includes(3);
+
+console.log(checkForThree(numbers1));
+console.log(checkForThree(numbers2));
+console.log(checkForThree(numbers3));
