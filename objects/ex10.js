c47e0cca15c102de48e693f5942121faacdd62ef
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-13 14:23:14 -0500

    solved ex10

---
+++ objects/ex10.js
@@ -0,0 +1,13 @@
+[1, 2, ["a", ["b", false]], null, {}]
+// primitive - number
+// primitive - number
+// object - array
+  // primitive - string
+  // object - array
+    // primitive -string
+    // primitive boolean
+// primitive - object (null)
+// object
+
+// 6 primitive
+// 3 objects
