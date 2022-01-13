d3d703c5895664b651ba00c6652f2ff91227cd98
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-13 09:58:07 -0500

    solved ex2

---
+++ loops-n-iters/factorial.js
@@ -0,0 +1,17 @@
+const factorial = n => {
+  // Returns the factorial of n
+  let result = 1;
+  for (let i = n; i > 0; i -= 1) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
