20bfa7c826a01d2907665ef49c1b5defea85906e
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-14 11:16:24 -0500

    solved ex1

---
+++ js100-ex/reading-docs2/ex1.js
@@ -0,0 +1,8 @@
+const iceCreamTaste = 'chocolate';
+let iceCreamDensity = 10;
+
+while (iceCreamDensity > 0) {
+  console.log('Drip...');
+  iceCreamDensity -= 1;
+}
+console.log(`The ${iceCreamTaste} ice cream melted.`);
