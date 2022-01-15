f89b369f662d4ee5b88eee584293edafab5a1e83
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-15 15:35:38 -0500

    solved ex5

---
+++ js100-ex/conds/ex5.js
@@ -0,0 +1,21 @@
+let animal = 'horse';
+
+switch (animal) {
+  case 'duck':
+    console.log('quack');
+  case 'squirrel':
+    console.log('nook nook');
+  case 'horse':
+    console.log('neigh');
+  case 'bird':
+    console.log('tweet tweet');
+  default:
+    console.log('*cricket*');
+}
+
+// neight
+// tweet tweet
+// *cricket*
+
+// as there is no break so all the statements after the initial case match
+// execute
