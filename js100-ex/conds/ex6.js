30391b43c50ec65b44bb35003648db600f6ca85d
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-15 15:39:01 -0500

    solved ex6

---
+++ js100-ex/conds/ex6.js
@@ -0,0 +1,15 @@
+const weather = 'snowy';
+
+switch (weather) {
+  case 'sunny':
+    console.log('It\'s a beautiful day!');
+    break;
+  case 'rainy':
+    console.log('Grab your umbrella');
+    break;
+  case 'snowy':
+    console.log('lets go have fun!');
+    break;
+  default:
+    console.log('Let\'s stay inside.');
+}
