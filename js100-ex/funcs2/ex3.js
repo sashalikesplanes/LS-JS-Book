ba855cc50e1f3e0acf421b6a5e23573838cb105d
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-16 02:34:15 -0500

    solved 3

---
+++ js100-ex/funcs2/ex3.js
@@ -0,0 +1,11 @@
+function greeting() {
+  return 'Good morning';
+}
+
+function recipient() {
+  return 'Launch School';
+}
+
+const greet = () => console.log(`${greeting()}, ${recipient()}!`);
+
+greet();
