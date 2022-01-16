194f903111b4b23dec9e8b7f53f90e0df2d9a746
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-16 01:48:50 -0500

    solved ex7

---
+++ js100-ex/arrs/ex7.js
@@ -0,0 +1,7 @@
+let array1 = [2, 6, 4];
+let array2 = [2, 6, 4];
+
+console.log(array1 === array2);
+
+// false, as arrays are objects and objects are compared by reference, here
+// the two arrays have difference arrays in memory as they are assingned separatly
