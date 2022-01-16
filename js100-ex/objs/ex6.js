108821ae0f36c6210c2197af5c514d8a5e6b1041
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-16 02:19:11 -0500

    solved 6 and 7

---
+++ js100-ex/objs/ex6.js
@@ -0,0 +1,17 @@
+let obj = {
+  num: 42,
+  'property name': 'string value',
+  true: false,
+  fun: function() {
+    console.log('Harr Harr!');
+  },
+};
+
+for (let prop in obj) {
+  if (prop === true) {
+    console.log("It's true!");
+  }
+}
+
+// It's true is never outputted as the prop is stored as a string
+// which is not strictly equal to the boolean true
