208bd891d9379d8e9ca8cfc09b221a37da06edb1
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-16 02:03:47 -0500

    solved 11

---
+++ js100-ex/arrs/ex11.js
@@ -0,0 +1,7 @@
+let groceryList = ['paprika', 'tofu', 'garlic', 'quinoa', 'carrots', 'broccoli', 'hummus'];
+
+while (groceryList.length > 0) {
+  console.log(groceryList.shift());
+}
+console.log(groceryList);
+
