561f759cba2b388ef75d1a1a13d2fb0042b40e9e
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-16 02:07:30 -0500

    solved 1, 2, 3

---
+++ js100-ex/objs/ex3.js
@@ -0,0 +1,13 @@
+let fido = {
+  name: 'Fido',
+  species: 'Labrador Retriever',
+  color: 'brown',
+  weight: 16,
+};
+
+// Add property 'age'.
+// Add property 'favorite food'.
+fido.age = 69;
+fido['favorite food'] = 'bacon';
+
+console.log(fido);
