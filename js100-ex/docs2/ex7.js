f1afb1cf1105d2b28a57d432fa088fcdae7e7568
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-16 22:29:08 +0100

    solved 10 and 7

---
+++ js100-ex/docs2/ex7.js
@@ -0,0 +1,6 @@
+// It expects 1 argument
+// If you call it with zero arguments the default of ',' is used
+
+console.log(['Sasha', 'Ugo', 'Bob', 'Bill'].join(',', '!'));
+
+// Prediction, second argument gets ignored - yes!
