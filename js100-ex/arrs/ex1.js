64e3a293e0771a374df16c1e7a17f137fab52c44
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-01-16 01:36:32 -0500

    solved ex1 and ex2

---
+++ js100-ex/arrs/ex1.js
@@ -0,0 +1,7 @@
+const first = arr => arr[0];
+
+first(['Earth', 'Moon', 'Mars']); // 'Earth'
+console.log(first(['Earth', 'Moon', 'Mars']));
+console.log(first([]));
+
+// if array is empty first would return undefined
---
+++ js100-ex/arrs/ex2.js
@@ -0,0 +1,4 @@
+const last = array => array[array.length - 1];
+
+last(['Earth', 'Moon', 'Mars']); // 'Mars'
+console.log(last(['Earth', 'Moon', 'Mars']));
