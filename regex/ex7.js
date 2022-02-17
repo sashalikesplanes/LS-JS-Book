b5382da996b1c8aaeb84cc8766e5646dd970d9da
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-17 14:35:43 +0100

    solve exercise

---
+++ regex/ex7.js
@@ -0,0 +1,6 @@
+const formatDate = (string) =>
+  string.replace(/(\d{4})([\/\-])(\d{2})\2(\d{2})/, "$4.$3.$1");
+
+console.log(formatDate("2016-06-17")); // -> '17.06.2016'
+console.log(formatDate("2017/05/03")); // -> '03.05.2017'
+console.log(formatDate("2015/01-31")); // -> '2015/01-31' (no change)
