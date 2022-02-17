9a011292a62883cf8e20dcaf89352cc12437524a
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-17 14:13:55 +0100

    solved ex

---
+++ regex/ex1.js
@@ -0,0 +1,6 @@
+const isUrl = (possibleUrl) => !!possibleUrl.match(/^https?:\/\/\S+$/);
+
+console.log(isUrl("http://launchschool.com")); // -> true
+console.log(isUrl("https://example.com")); // -> true
+console.log(isUrl("https://example.com hello")); // -> false
+console.log(isUrl("   https://example.com")); // -> false
