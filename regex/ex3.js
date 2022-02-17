a5adc6dd524945812f6169c8acc098e54c73e829
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-17 14:22:31 +0100

    solved exercise

---
+++ regex/ex3.js
@@ -0,0 +1,7 @@
+const mysteryMath = (string) => string.replace(/[+\-*/]/, "?");
+
+console.log(mysteryMath("4 + 3 - 5 = 2"));
+// -> '4 ? 3 - 5 = 2'
+
+console.log(mysteryMath("(4 * 3 + 2) / 7 - 1 = 1"));
+// -> '(4 ? 3 + 2) / 7 - 1 = 1'
